bfd749d7c02dc6d2997b450fb81cec274081e86f
Victoria Rozhina <sandaar@users.noreply.github.com>
2018-10-24 16:52:45 -0700

    Enable govcloud (#2)
    * Use AWS::Partition pseudo parameter for ARNs. That will take into
    account aws,  aws-us-gov, for example, and others too.
    Added fake mappings in buckets because cloudformation breaks otherwise.

--- lambda/snapshots_tool_utils.py
+++ lambda/snapshots_tool_utils.py
@@ -153,7 +153,7 @@ def get_shared_snapshots(pattern, response):
 
 def get_snapshot_identifier(snapshot):
 # Function that will return the RDS Snapshot identifier given an ARN
-    match = re.match('arn:aws:rds:.*:.*:snapshot:(.+)',
+    match = re.match('arn:aws.*:rds:.*:.*:snapshot:(.+)',
                      snapshot['DBSnapshotArn'])
     return match.group(1)
 
